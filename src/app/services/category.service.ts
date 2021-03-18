e696ea9f631f518cf61186dd13806e0b7a30107c
Onur Budak <onurbudak1997@outlook.com>
2021-03-18 21:47:55 +0300

    18 Mart 2021

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ListResponseModel } from '../models/ListResponseModel';
 import { Category } from '../models/category';
+import { ListResponseModel } from '../models/listResponseModel';
 
 @Injectable({
   providedIn: 'root'
